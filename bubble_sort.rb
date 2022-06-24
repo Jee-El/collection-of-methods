548b19d96a0f2ac22f11b5e571cadbae6c84274d
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-06-24 16:26:00 +0100

    Implement bubble_sort to sort numbers by size

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(arr)
+  swaped = -1
+  until (swaped == 0)
+    swaped = 0
+    i = 0
+    unsorted_length = arr.length
+    while ( i + 1 < unsorted_length )
+      if arr[i] > arr[i+1]
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        swaped = 1
+      end
+      i += 1
+    end
+    unsorted_length -= 1
+  end
+  arr
+end
+
+p bubble_sort([4,3,78,2,0,2]) # => [0, 2, 2, 3, 4, 78]
