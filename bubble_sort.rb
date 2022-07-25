6d63002833bca62f748dc5698d662d18d64b5b2b
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-07-25 20:06:07 +0100

    Remove debugging code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,4 @@
 def bubble_sort(arr)
-  # swaped = -1
   swapped = true
   while swapped
     swapped = false
@@ -17,4 +16,4 @@ def bubble_sort(arr)
   arr
 end
 
-p bubble_sort([4, 3, 78, 2, 0, 2]) # => [0, 2, 2, 3, 4, 78]
+bubble_sort([4, 3, 78, 2, 0, 2]) # => [0, 2, 2, 3, 4, 78]
