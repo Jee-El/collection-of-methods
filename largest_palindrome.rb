095ce12258a4ec05950f2cbfd04f955f1493f9af
jee-el <98971420+Jee-El@users.noreply.github.com>
2022-09-25 19:21:29 +0100

    Add largest_palindrome

---
+++ largest_palindrome.rb
@@ -0,0 +1,34 @@
+# Uses tail recursion
+RubyVM::InstructionSequence.compile_option = {
+  tailcall_optimization: true,
+  trace_instruction: false
+}
+
+RubyVM::InstructionSequence.new(<<~EOF).eval
+  def recursive_largest_palindrome(num = 999 * 999, factor = 999)
+    if num.to_s.reverse.to_i == num
+      q, r = num.divmod(factor)
+      return num if q.to_s.length == 3 && r.zero?
+
+      return recursive_largest_palindrome(num - 1, 999) if factor < 100
+
+      return recursive_largest_palindrome(num, factor - 1)
+    end
+    recursive_largest_palindrome(num - 1, factor)
+  end
+EOF
+
+recursive_largest_palindrome #=> 906609
+
+# Uses simple loops
+def loop_largest_palindrome(largest = 0)
+  100.upto(999) do |i|
+    100.upto(999) do |j|
+      p = i * j
+      largest = p if p == p.to_s.reverse.to_i && p > largest
+    end
+  end
+  largest
+end
+
+loop_largest_palindrome #=> 906609
