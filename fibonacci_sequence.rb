e8873eb63d322544b771878740241a00894d2d2a
Jee El <98971420+Jee-El@users.noreply.github.com>
2023-01-23 02:26:25 +0100

    One less line

--- fibonacci_sequence.rb
+++ fibonacci_sequence.rb
@@ -11,9 +11,7 @@ nth_fibonacci_number(8) #=> 13
 def fibonacci_sequence(n, a = 0, b = 1, sequence = [])
   return sequence if n.zero?
 
-  sequence << a
-
-  fibonacci_sequence(n - 1, b, a + b, sequence)
+  fibonacci_sequence(n - 1, b, a + b, sequence + [a])
 end
 
 fibonacci_sequence(8) #=> [0, 1, 1, 2, 3, 5, 8, 13]
